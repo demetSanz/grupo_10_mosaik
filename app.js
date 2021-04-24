4a444cfe8f0097b3e1cbdb7f83d09a1df9622c58
Salvucci Carlos <salvuccicarlos@gmail.com>
2021-04-24 12:45:13 -0300

    Modificaciones a app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // Requerimos express
+const app = express(); // Llamamos express dentro de la constante app
+const port = 3003; // Colocamos el puerto en una constante para que pueda modificarse facilmente
 
-app.listen(3003, () => 
-console.log('Servidor corriendo Grupo 10 DH')
+app.listen(port, () => 
+console.log('Servidor corriendo Grupo 10 DH en el puerto '+port)
 );
 
 app.get('/home', (req, res) => {
