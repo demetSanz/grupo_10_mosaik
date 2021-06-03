6bf208d5aae5304a52a67449ee9827e4968d2ad6
luis crescente <luis-94@live.com.ar>
2021-06-03 16:15:41 -0300

    page 404

--- src/app.js
+++ src/app.js
@@ -28,6 +28,10 @@ app.use('/', routerMain);
 app.use('/products',routersProducts);
 app.use ('/user', routerUsers)
 
+app.use((req, res, next)=>{
+    res.status(404).render('error404');
+});
+
 
 // app.use('users', usersRoutes);
 // app.use('products', productsRoutes);
