71e74c07bbad431663b33dc362a22449a58a76dd
luisCrescente <luis94.lc@gmail.com>
2021-06-17 15:28:55 -0300

    cambios en userLoggedMiddleware

--- src/middleware/userLoggedMiddleware.js
+++ src/middleware/userLoggedMiddleware.js
@@ -1,6 +1,9 @@
 function userLoggedMiddleware(req, res, next) {
 	res.locals.isLogged = false;
-    
+    if (req.session.userLogged) {
+		res.locals.isLogged = true;
+		res.locals.userLogged = req.session.userLogged;
+	}
 
 	next();
 }
