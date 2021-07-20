1329c079e2aeddeac9605b784cc84d3385eb6c6e
Salvucci Carlos <salvuccicarlos@gmail.com>
2021-07-20 17:08:46 -0300

    Correccion de multiples modelos consultados para formulario de creacion de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,19 +4,24 @@ const db = require("../database/models");
 const productController ={
 
     pedro:function (req,res){
-            db.Category.findAll()
-                .then(function(category){
-                 return res.render('creacionProducto',{category});
-                })
-           db.Size.findAll()
-                .then(function(sizes){
-                   return res.render('creacionProducto',{sizes})
-            })
+        
+        let categories = db.Category.findAll()
+        let sizes = db.Size.findAll()
+
+        Promise
+        .all([categories, sizes])
+        .then(
+            function(responses){
+                let category = responses[0];
+                let sizes = responses[1];
+                // console.log('**********COMPLETE RESULTS****************');
+                // console.log(category[0].name); 
+                // console.log(sizes[0].name); 
+                return res.render('creacionProducto',{category,sizes})
+
+            }
+        )
     }
-
-
 }
-
-
 
 module.exports =productController ;
