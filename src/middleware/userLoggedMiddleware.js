9aaed8d745ba21a91b85a7a865c1dd32caede9f4
Salvucci Carlos <salvuccicarlos@gmail.com>
2021-07-24 12:46:10 -0300

    Profile corregido

--- src/middleware/userLoggedMiddleware.js
+++ src/middleware/userLoggedMiddleware.js
@@ -5,23 +5,24 @@ function userLoggedMiddleware(req, res, next) {
 
 	let loggedUser = req.session.userLogged;
 	
-	console.log(loggedUser + " estoy aca");
-	
 	db.User.findOne({
 			where:{
 				email: loggedUser
 			}
 		})
-		.then(function(response){
-		//	console.log(userFromCookie)
-			let userFromCookie = response[0]
-			if(userFromCookie){
-			req.session.userLogged=userFromCookie;
+		.then(function(loggedUser){
+		
+			let UserFound = loggedUser
+			// console.log(UserFound.name + " estoy aca")
+
+			if(UserFound){
+			req.session.userLogged = UserFound;
 			}
 
     		if (req.session.userLogged) {
 			res.locals.isLogged = true;
-			res.locals.userLogged = req.session.userLogged;
+			res.locals.userLogged = UserFound;
+			
 			}
 		}
 	);
