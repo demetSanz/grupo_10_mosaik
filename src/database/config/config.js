44fb09083557fdebdab4e5fc045404c7b8613d09
Salvucci Carlos <salvuccicarlos@gmail.com>
2021-07-27 22:24:13 -0300

    se agrega dotenv para comodidad de los developers

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": process.env.DB_PASS,
     "database": "mosaik_db",
     "host": "127.0.0.1",
-    "port": "8889",
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "test": {
