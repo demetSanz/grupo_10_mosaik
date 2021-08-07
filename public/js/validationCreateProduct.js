544c1f10aaa4af88bb98c7f578051aa97677c5ef
luisCrescente <luis94.lc@gmail.com>
2021-08-07 17:46:35 -0300

    validacion de jsFront al crear un producto

---
+++ public/js/validationCreateProduct.js
@@ -0,0 +1,72 @@
+window.onload = function(){
+    let product = document.querySelector('#nameProduct');
+    product.focus();    
+    let form = document.querySelector('#form')
+    
+    form.addEventListener('submit',function(e){
+        
+        let errors =[];
+
+    let product = document.querySelector('#nameProduct');
+    let price = document.querySelector('#price');
+    let description = document.querySelector('#description');
+    let stock = document.querySelector('#stock')
+        
+    if(product.value == ""){
+        product.classList.add('is-invalid');
+        errors.push('El campo no puede estar vacio');
+
+    }else if (product.value.length < 5){
+        product.classList.add('is-invalid');
+        errors.push('El campo debe tener minimo 5 caracteres')
+    }else{
+        product.classList.remove('is-invalid');
+        product.classList.add('is-valid');
+        price.focus();  
+    };
+
+    if(price.value == ""){
+        price.classList.add('is-invalid');
+        errors.push('El campo debe tener un precio')
+    }else{
+        price.classList.remove('is-invalid');
+        price.classList.add('is-valid');
+        description.focus();
+    };
+
+    if(description.value == ""){
+        description.classList.add('is-invalid');
+        errors.push('La descripcion no puede estar vacia')
+    }else if (description.value.length < 20){
+
+        description.classList.add('is-invalid');
+        errors.push('El campo debe tener al menos 20 caracteres')
+    }else{
+        description.classList.remove('is-invalid');
+        description.classList.add('is-valid');
+        stock.focus();
+    }
+
+    if(stock.value == ""){
+        stock.classList.add('is-invalid');
+        errors.push('Debe ingresar una cantidad')
+    }else{
+        stock.classList.remove('is-invalid');
+        stock.classList.add('is-valid');
+        description.focus();
+    };
+
+    if(errors.length >0){
+        e.preventDefault();
+        let MessagesErrors = document.querySelector('.errors');
+        MessagesErrors.innerHTML ='';
+        MessagesErrors.classList.add('warning');
+        for(let i = 0; i <errors.length; i++){
+        MessagesErrors.innerHTML += `<li> ${errors[i]} <li>`
+        }
+    }else{
+        alert('subio con exito');
+        //form.submit();
+    }
+    })
+}
