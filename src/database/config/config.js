a458b24d1923d5196dd06ba59a0cba0f23e4dda0
NahuiiGHm <nggh@hotmail.com>
2021-08-30 13:10:10 -0300

    subiendo algunas mejoras finales

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
-    "port":8889,
+    "password": null,
+    "port":3306,
     "database": "mosaik_db",
     "host": "127.0.0.1",
     
